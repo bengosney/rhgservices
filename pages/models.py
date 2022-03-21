85fc2c46e0678ac8eb6a497002579e439537f648
Ben Gosney <bengosney@googlemail.com>
2022-03-21 21:43:08 +0000

    better styling and banner image

--- pages/models.py
+++ pages/models.py
@@ -1,14 +1,19 @@
-# Third Party
+# Django
+from django.db import models
+
+# Wagtail
 from wagtail.admin.edit_handlers import StreamFieldPanel
 from wagtail.core import blocks
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 # Locals
 from .blocks import CallOutBlock, FlexBlock, InfoPodBlock
 
 
 class HomePage(Page):
+    banner_image = models.ForeignKey("wagtailimages.Image", null=True, blank=False, on_delete=models.SET_NULL, related_name="+")
     body = StreamField(
         [
             ("Callout", CallOutBlock()),
@@ -18,6 +23,7 @@ class HomePage(Page):
     )
 
     content_panels = Page.content_panels + [
+        ImageChooserPanel("banner_image"),
         StreamFieldPanel("body"),
     ]
 
