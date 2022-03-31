221456a357c44360c03cc07794fa8be0c9814a62
Ben Gosney <bengosney@googlemail.com>
2022-03-31 14:04:38 +0100

    Add sitemap

--- rhgs/urls.py
+++ rhgs/urls.py
@@ -5,6 +5,7 @@ from django.urls import include, path
 
 # Wagtail
 from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.contrib.sitemaps.views import sitemap
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
@@ -16,6 +17,7 @@ urlpatterns = [
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),
+    path("sitemap.xml", sitemap),
 ]
 
 
