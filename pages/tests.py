ca17c68916a04af2286227814ade2f9f0ec7f948
Ben Gosney <bengosney@googlemail.com>
2022-04-04 13:39:40 +0100

    Better can not create test

--- pages/tests.py
+++ pages/tests.py
@@ -3,12 +3,12 @@ from wagtail.core.models import Page
 from wagtail.tests.utils import WagtailPageTests
 
 # Locals
-from .models import HomePage, InfoPage
+from .models import HomePage
 
 
 class HomePageTests(WagtailPageTests):
     def test_can_not_create_homepage(self):
-        self.assertCanNotCreateAt(InfoPage, HomePage)
+        self.assertAllowedParentPageTypes(HomePage, [Page])
 
     def test_can_create_homepage(self):
         self.assertCanCreateAt(Page, HomePage)
