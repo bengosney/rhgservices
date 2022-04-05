c570e8eecfdcc5cd64dade118b7c68765641ccb0
Ben Gosney <bengosney@googlemail.com>
2022-04-05 20:47:39 +0100

    Fixes Map needs google api key #12

--- pages/models.py
+++ pages/models.py
@@ -23,6 +23,7 @@ class SiteSettings(BaseSetting):
     phone_number = models.CharField(max_length=255, help_text="Phone number to show in the footer", default="")
     facebook = models.URLField(help_text="Your Facebook page URL", default="")
     instagram = models.CharField(max_length=255, help_text="Your Instagram username, without the @", default="")
+    google_maps_api_key = models.CharField(max_length=255, help_text="Google Maps API key", default="")
 
 
 class HomePage(Page):
