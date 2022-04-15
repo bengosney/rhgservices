6be76372d9fd091dbbd0aaf867e3297c19ace474
Ben Gosney <bengosney@googlemail.com>
2022-04-15 20:24:23 +0100

    Add the correct domain for csrf

--- rhgs/settings/base.py
+++ rhgs/settings/base.py
@@ -168,4 +168,5 @@ WAGTAILSEARCH_BACKENDS = {
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = "http://example.com"
+BASE_URL = "https://www.rhgservices.co.uk"
+CSRF_TRUSTED_ORIGINS = ["https://www.rhgservices.co.uk"]
