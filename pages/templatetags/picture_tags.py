d89ea35c31b9375acef770719b9f9f9f67f2f7c6
Ben Gosney <bengosney@googlemail.com>
2022-04-24 10:59:25 +0100

    Actualy use the new media queries

--- pages/templatetags/picture_tags.py
+++ pages/templatetags/picture_tags.py
@@ -120,7 +120,7 @@ class PictureNode(template.Node):
 
         srcsets = []
 
-        def getType(ext):
+        def get_type(ext):
             _type = "jpeg" if ext == "jpg" else ext.replace(".", "")
             return f"image/{_type}"
 
@@ -130,8 +130,8 @@ class PictureNode(template.Node):
                 _, extention = os.path.splitext(rendition.file.name)
                 attrs = [
                     f'srcset="{rendition.url}"',
-                    f'media="(max-width: {rendition.width}px)"',
-                    f'type="{getType(extention)}"',
+                    f'media="({get_media_query(spec, rendition)})"',
+                    f'type="{get_type(extention)}"',
                 ]
                 srcsets.append(f'<source {" ".join(attrs)} />')
 
@@ -140,7 +140,7 @@ class PictureNode(template.Node):
             _, extention = os.path.splitext(rendition.file.name)
             attrs = [
                 f'srcset="{rendition.url}"',
-                f'type="{getType(extention)}"',
+                f'type="{get_type(extention)}"',
             ]
             srcsets.append(f'<source {" ".join(attrs)} />')
             if base is None and extention in [".jpg", ".png"]:
