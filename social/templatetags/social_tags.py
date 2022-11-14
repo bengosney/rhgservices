7d7fbfa355fe1aa30d48c9c21222d40ad6921835
Ben Gosney <bengosney@googlemail.com>
2022-11-14 13:57:20 +0000

    Copy some improvments over from STL

---
+++ social/templatetags/social_tags.py
@@ -0,0 +1,32 @@
+# Django
+from django import template
+
+# Wagtail
+from wagtail.models import Site
+
+# Locals
+from ..models import Social
+
+register = template.Library()
+
+
+@register.inclusion_tag("tags/social_tags.html", takes_context=True)
+def social_tags(context):
+    site = Site.find_for_request(context["request"])
+    settings = Social.for_request(context["request"])
+
+    context["site"] = site
+
+    try:
+        jsonld = template.Template(settings.json_ld)
+        rendered_jsonld = jsonld.render(context)
+    except Exception:
+        rendered_jsonld = ""
+
+    return {
+        "og_description": settings.description,
+        "og_image": settings.image,
+        "og_title": site.site_name,
+        "og_url": site.root_url,
+        "jsonld": rendered_jsonld,
+    }
