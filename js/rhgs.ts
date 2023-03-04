bc4755c657c9dab44cf10e2541a4034d24210221
Ben Gosney <bengosney@googlemail.com>
2023-03-04 17:56:15 +0000

    Better pre loader, dont mess with the dom as much

--- js/rhgs.ts
+++ js/rhgs.ts
@@ -1,3 +1,6 @@
 import initSlider from "./sliders";
+import initPreloads from "./pre-load";
 
 Array.from(document.getElementsByClassName("slider")).map((e) => initSlider(e, 2500));
+
+initPreloads();
