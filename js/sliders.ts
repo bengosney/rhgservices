79a800fab76a2ca9bb300223b56db5be47d5ca73
Ben Gosney <bengosney@googlemail.com>
2023-03-09 21:44:50 +0000

    WIP: swipe

--- js/sliders.ts
+++ js/sliders.ts
@@ -1,5 +1,8 @@
 const initSlider = (slider: Element, duration: number) => {
     const select = (selector: string) => slider.querySelector<HTMLInputElement>(selector);
+
+    const nextSlide = () => select("input:checked ~ input") || select("input:first-child");
+
     const initTimer = () =>
         setInterval(() => {
             const next = select("input:checked ~ input") || select("input:first-child");
@@ -14,6 +17,29 @@ const initSlider = (slider: Element, duration: number) => {
 
     slider.addEventListener("mouseover", () => clearInterval(timer), true);
     slider.addEventListener("mouseout", () => (timer = initTimer()), true);
+
+    let startEvent: TouchEvent | undefined = undefined;
+    slider.addEventListener("touchstart", (e) => (startEvent = e as TouchEvent), true);
+    slider.addEventListener(
+        "touchend",
+        (e) => {
+            const endEvent = e as TouchEvent;
+            if (startEvent) {
+                const timeDiff = endEvent.timeStamp - startEvent.timeStamp;
+                const { touches } = startEvent;
+                const { changedTouches } = endEvent;
+                const xDiff = changedTouches[0].screenX - touches[0].screenX;
+                const yDiff = changedTouches[0].screenY - touches[0].screenY;
+
+                if (timeDiff <= 500 && Math.abs(xDiff) > Math.abs(yDiff * 1.5) && Math.abs(xDiff) > 100) {
+                    if (xDiff > 0) {
+                        nextSlide()?.checked;
+                    }
+                }
+            }
+        },
+        true,
+    );
 };
 
 export default initSlider;
