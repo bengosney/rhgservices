add3dda506836b2b14bf37c5245d7e2b90402e16
Ben Gosney <bengosney@googlemail.com>
2023-05-04 09:00:57 +0100

    Test the map block

--- pages/tests.py
+++ pages/tests.py
@@ -8,6 +8,7 @@ from wagtail.models import Page
 from wagtail.test.utils import WagtailPageTests
 
 # First Party
+from pages.blocks import MapBlock
 from pages.models import FormPage
 
 # Locals
@@ -49,3 +50,24 @@ class FormPageTestCase(TestCase):
         self.assertIsInstance(data_fields, list)
         for data_field in data_fields:
             self.assertIsInstance(data_field, tuple)
+
+
+class MapBlockTestCase(TestCase):
+    def setUp(self) -> None:
+        self.values = {"title": "Test Map", "latitude": 40.712776, "longitude": -74.005974}
+        self.hash = "0a2ca8e1d6f9c5c7ad9c5a8996768610"
+        return super().setUp()
+
+    def test_get_context(self):
+        block = MapBlock()
+        context = block.get_context(self.values)
+        self.assertIn("self", context)
+        self.assertIn("hash", context["self"])
+        self.assertEqual(context["self"]["title"], "Test Map")
+        self.assertEqual(context["self"]["latitude"], 40.712776)
+        self.assertEqual(context["self"]["longitude"], -74.005974)
+
+    def test_hash_generation(self):
+        block = MapBlock()
+        context1 = block.get_context(self.values)
+        self.assertEqual(context1["self"]["hash"], self.hash)
