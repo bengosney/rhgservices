44f38a548cd239451ae192da716028c789e35a87
Ben Gosney <bengosney@googlemail.com>
2023-05-09 13:35:08 +0100

    Test is_active

--- pages/tests/test_navigation_tags.py
+++ pages/tests/test_navigation_tags.py
@@ -3,10 +3,10 @@ from django.test import RequestFactory, TestCase
 
 # First Party
 from pages.models import InfoPage
-from pages.templatetags.navigation_tags import get_site_root, has_children, has_menu_children
+from pages.templatetags.navigation_tags import get_site_root, has_children, has_menu_children, is_active
 
 
-class GetSiteRootTestCase(TestCase):
+class NavigationTagsTests(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.root_page = InfoPage(title="Home", path="/", depth=1)
@@ -35,3 +35,11 @@ class GetSiteRootTestCase(TestCase):
     def test_has_children_no_children(self):
         result = has_children(self.info_page)
         self.assertFalse(result)
+
+    def test_is_active(self):
+        active = is_active(self.info_page, self.info_page)
+        self.assertTrue(active)
+
+    def test_not_is_active(self):
+        active = is_active(self.info_page, self.root_page)
+        self.assertTrue(active)
