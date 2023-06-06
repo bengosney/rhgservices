8b2447c73ed8a7e4a036c38b8f6f85e968a93108
Ben Gosney <bengosney@googlemail.com>
2023-06-06 16:31:49 +0100

    Renable sliders and disable preload

--- js/rhgs.ts
+++ js/rhgs.ts
@@ -1,6 +1,6 @@
 import initSlider from "./sliders";
-import initPreloads from "./pre-load";
+//import initPreloads from "./pre-load";
 
-//Array.from(document.getElementsByClassName("slider")).map((e) => initSlider(e, 2500));
+Array.from(document.getElementsByClassName("slider")).map((e) => initSlider(e, 2500));
 
-initPreloads();
+//initPreloads();
