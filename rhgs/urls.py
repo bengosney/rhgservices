2da47706dfee321acb4024b1b2112240c5bd0ef6
Ben Gosney <bengosney@googlemail.com>
2023-08-07 12:23:28 +0100

    Update everything

--- rhgs/urls.py
+++ rhgs/urls.py
@@ -9,9 +9,6 @@ from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.contrib.sitemaps.views import sitemap
 from wagtail.documents import urls as wagtaildocs_urls
 
-# First Party
-from search import views as search_views
-
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
