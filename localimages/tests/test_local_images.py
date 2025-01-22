20d22b26a314452839d69c69ef2621bb68dbe2c9
Ben Gosney <bengosney@googlemail.com>
2025-01-22 21:25:11 +0000

    refactor: standardize variable naming for improved consistency and readability

--- localimages/tests/test_local_images.py
+++ localimages/tests/test_local_images.py
@@ -21,19 +21,19 @@ def cant_mkdtemp():
 
 class LocalImagesTestCase(TestCase):
     def setUp(self) -> None:
-        self.tmpDir = tempfile.mkdtemp(dir=settings.MEDIA_ROOT)
+        self.tmp_dir = tempfile.mkdtemp(dir=settings.MEDIA_ROOT)
 
     def get_tmp_name(self):
         for i in range(1000):
-            filePath = path.join(self.tmpDir, f"no-{i}-img.jpg")
-            if not path.exists(filePath):
-                return filePath
+            file_path = path.join(self.tmp_dir, f"no-{i}-img.jpg")
+            if not path.exists(file_path):
+                return file_path
         raise Exception("No free file found")
 
     @skipIf(cant_mkdtemp(), "Can't make a temp file")
     def test_fill_stock_image(self):
-        imgPath = self.get_tmp_name()
+        img_path = self.get_tmp_name()
 
-        fill_image_with_stock(imgPath)
+        fill_image_with_stock(img_path)
 
-        self.assertTrue(path.exists(imgPath))
+        self.assertTrue(path.exists(img_path))
