ee76c7e524e4664c4bf7ac2774430a471195cf14
Ben Gosney <bengosney@googlemail.com>
2025-01-22 21:22:11 +0000

    refactor(urls): remove commented-out search path and unnecessary comments for clarity

--- rhgs/urls.py
+++ rhgs/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
-    # path("search/", search_views.search, name="search"),
     path("sitemap.xml", sitemap),
     path("robots.txt", include("robots.urls")),
     path("health/", include("health_check.urls")),
@@ -25,17 +24,10 @@ if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-    # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
 
 urlpatterns += [
-    # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
-    # the list:
     path("", include(wagtail_urls)),
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    path("pages/", include(wagtail_urls)),
 ]
