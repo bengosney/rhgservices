af655b2a7d4a81748fda2f531235cd7e37a1d73e
Ben Gosney <bengosney@googlemail.com>
2026-03-01 12:45:05 +0000

    build(assets): rebuild static assets

--- rhgs/static/js/rhgs.js
+++ rhgs/static/js/rhgs.js
@@ -1,2 +1,2 @@
-Array.from(document.getElementsByClassName("slider")).map(e=>{let t;return t=!1,e.addEventListener("mouseover",()=>t=!0,!0),e.addEventListener("mouseout",()=>t=!1,!0),setInterval(()=>{let r=e.querySelector("input:checked ~ input")||e.querySelector("input:first-child");0==document.querySelectorAll(".lightbox input:checked").length&&!t&&r&&(r.checked=!0)},2500)});
+Array.from(document.getElementsByClassName("slider")).map(e=>{let t;return t=!1,e.addEventListener("mouseover",()=>t=!0,!0),e.addEventListener("mouseout",()=>t=!1,!0),setInterval(()=>{let r=e.querySelector("input:checked ~ input")||e.querySelector("input:first-child");0==document.querySelectorAll("dialog[open].lightbox").length&&!t&&r&&(r.checked=!0)},2500)});
 //# sourceMappingURL=rhgs.js.map
